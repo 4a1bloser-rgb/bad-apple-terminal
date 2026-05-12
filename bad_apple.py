33115abda95c0da460f848179c63e53cec9309f3
4a1bloser-rgb <4a1b.loser@gmail.com>
2026-05-12 18:43:25 +0800

    Update bad_apple.py

--- bad_apple.py
+++ bad_apple.py
@@ -13,7 +13,7 @@ fps = 30
 frame_time = 1 / fps
 
 def play_music(): 
-    os.system("start bad_apple.mp3")
+    os.system("start bad apple.mp3")
 music_thread = threading.Thread( target=play_music, daemon=True )
 music_thread.start()
 
